f38bcad95f6840617936562df2e5d02d8c3b0c6d
Dan Fyfe <fyfedaniel@gmail.com>
2024-02-06 13:58:32 -0500

    wipe before conversion

--- coconuts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from coconuts import views
-
-# URL conf module - url config
-urlpatterns = [
-    path('', views.get_pages),
-    path('/<str:slug>', views.get_page_by_slug)
-]
