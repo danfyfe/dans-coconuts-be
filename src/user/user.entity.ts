66ea11eb874e34360125ef8f736c614683f5154a
Dan Fyfe <fyfedaniel@gmail.com>
2024-04-11 12:00:47 -0400

    add bcrypt

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,12 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { PreSaveMiddlewareFunction } from 'mongoose';
+import { hash } from 'bcrypt';
 
 @Schema()
 export class UserEntity {
   @Prop()
   username: string;
 
-  @Prop({ select: false })
+  @Prop({ select: false, required: true })
   password: string;
 }
 
 export const UserEntitySchema = SchemaFactory.createForClass(UserEntity);
+
+UserEntitySchema.pre<UserEntity>('save', async (next: any) => {
+  this.password = await hash(this.password, 10);
+  next();
+});
